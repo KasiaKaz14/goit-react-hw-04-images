3e64b6e19ceffed43dda8099248ce3ae2e7b6678
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-09-24 18:51:08 +0200

    e

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,10 +2,7 @@ import React, { useState } from 'react';
 import css from './ImageGalleryItem.module.css';
 
 export const ImageGalleryItem = ({ src, alt, largeImageURL, openModal }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   const handleClick = () => {
-    setIsModalOpen(true);
     openModal(largeImageURL);
   };
 
